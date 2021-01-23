0e602ec4c511d11d2e7611616e6cc3798064ac9a
Damien Corona <damien.corona@wizbii.com>
2021-01-23 11:10:50 +0100

    add function to insert reparation

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ api = Api(app);
 CORS(app);
 
 class Cars(Resource):
-	def get(self):
+	@app.route("/only-cars", methods=["GET"])
+	def getCars():
 		conn = db_connect.connect()
-		query = conn.execute("select id, name, brand, color, immatriculation, controle_technique from car")
+		query = conn.execute("SELECT id, name, brand, color, immatriculation, controle_technique FROM car")
 		cars = [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]
 		for car in cars:
 			for key, value in car.items():
@@ -24,8 +25,22 @@ class Cars(Resource):
 		result = {'cars': cars}
 		return jsonify(result)
 
+class CarsWithReparations(Resource):
+	@app.route("/cars", methods=["GET"])
+	def getCarsAndReparations():
+		conn = db_connect.connect()
+		query = conn.execute("SELECT id, name, brand, color, immatriculation, controle_technique FROM car")
+		cars = [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]
+		for car in cars:
+			query = conn.execute("SELECT id, type, kilometrage, description, adresse_garage, cout, date FROM reparation WHERE car_id=%d ORDER BY date" %int(car['id']))
+			reparations =[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
+			car['reparations'] = reparations		
+		result = {'cars': cars}
+		return jsonify(result)
+		
 class Car(Resource):
-	def get(self, car_id):
+	@app.route("/car", methods=["GET"])
+	def getCar(car_id):
 		conn = db_connect.connect()
 		query = conn.execute("select id, name, color, immatriculation, controle_technique from car where id=%d " %int(car_id))
 		cars = [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]
@@ -35,10 +50,11 @@ class Car(Resource):
 					car[key] = datetime.fromtimestamp(value).strftime("%d/%m/%Y")
 		return jsonify(cars[0])
 
-class Reparations(Resource):
-	def get(self, car_id):
+class ReparationsByCar(Resource):
+	@app.route("/reparations/<car_id>", methods=["GET"], endpoint="get")
+	def getReparationsByCar(car_id):
 		conn = db_connect.connect()
-		query = conn.execute("select id, type, kilometrage, description, adresse_garage, cout, date from reparation WHERE car_id=%d " %int(car_id))
+		query = conn.execute("select id, type, kilometrage, description, adresse_garage, cout, date from reparation WHERE car_id=%d ORDER BY date" %int(car_id))
 		reparations =[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
 		for reparation in reparations:
 			for key, value in reparation.items():
@@ -47,10 +63,30 @@ class Reparations(Resource):
 		result = {'reparations': reparations}
 		return jsonify(result)
 
+class PostReparation(Resource): 
+	@app.route("/reparation", methods=["POST"], endpoint="post")	
+	def addReparation():
+		request_data = request.json
+		conn = db_connect.connect()
+		date = str(request_data['date']).split('/')
+		timestamp = datetime.fromisoformat(date[2] + "-" + date[1] + "-" + date[0]).timestamp()
+		query = conn.execute('INSERT INTO reparation (car_id, type, kilometrage, description, adresse_garage, cout, date) VALUES ({}, "{}", {}, "{}", "{}", {}, {})'.format(int(request_data['car_id']), str(request_data['type']), int(request_data['kilometrage']), str(request_data['description']).replace("'","\'"), str(request_data['adresse_garage']).replace("'","\'"), float(request_data['cout']), int(timestamp)))
+		result = {}
+		return jsonify(result)
 
-api.add_resource(Cars, '/cars')
-api.add_resource(Car, '/car/<car_id>')
-api.add_resource(Reparations, '/reparations/<car_id>')
+class Reparations(Resource):
+	@app.route("/reparations", methods=["GET"])
+	def getAllReparations(self):
+		conn = db_connect.connect()
+		query = conn.execute("select id, type, kilometrage, description, adresse_garage, cout, date from reparation ORDER BY date")
+		reparations =[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
+		for reparation in reparations:
+			for key, value in reparation.items():
+				if key == 'date':
+					reparation[key] = datetime.fromtimestamp(value).strftime("%d/%m/%Y")
+		result = {'reparations': reparations}
+		return jsonify(result)
+	
 
 if __name__ == '__main__':
 	app.run(port='5002')
