100f7d47f073e09759f7a8989a24d2c8562ea88d
Damien Corona <damien.corona@wizbii.com>
2021-07-12 10:43:19 +0200

    refactor code for easoer maintenance

--- app.py
+++ app.py
@@ -1,92 +1,165 @@
 #!/usr/bin/env python
-from flask import Flask, request
+import json
+
+from flask import Flask, request, Response
 from flask_restful import Resource, Api
-from sqlalchemy import create_engine
-from json import dumps
-from datetime import datetime
 from flask_jsonpify import jsonify
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
-db_connect = create_engine('sqlite:///car.db', connect_args={'check_same_thread': False});
-app = Flask(__name__);
-api = Api(app);
-CORS(app);
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///car.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 
 class Cars(Resource):
-	@app.route("/only-cars", methods=["GET"])
-	def getCars():
-		conn = db_connect.connect()
-		query = conn.execute("SELECT id, name, brand, color, immatriculation, controle_technique FROM car")
-		cars = [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]
-		for car in cars:
-			for key, value in car.items():
-				if key == 'controle_technique':
-					car[key] = datetime.fromtimestamp(value).strftime("%d/%m/%Y")
-		result = {'cars': cars}
-		return jsonify(result)
+    @app.route("/cars", methods=["GET"])
+    def get_cars():
+        cars = Car.query.all()
+        return jsonify([i.serialize for i in cars])
 
-class CarsWithReparations(Resource):
-	@app.route("/cars", methods=["GET"])
-	def getCarsAndReparations():
-		conn = db_connect.connect()
-		query = conn.execute("SELECT id, name, brand, color, immatriculation, controle_technique FROM car")
-		cars = [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]
-		for car in cars:
-			query = conn.execute("SELECT id, type, kilometrage, description, adresse_garage, cout, date FROM reparation WHERE car_id=%d ORDER BY date" %int(car['id']))
-			reparations =[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
-			car['reparations'] = reparations		
-		result = {'cars': cars}
-		return jsonify(result)
-		
-class Car(Resource):
-	@app.route("/car", methods=["GET"])
-	def getCar(car_id):
-		conn = db_connect.connect()
-		query = conn.execute("select id, name, color, immatriculation, controle_technique from car where id=%d " %int(car_id))
-		cars = [dict(zip(tuple(query.keys()) ,i)) for i in query.cursor]
-		for car in cars:
-			for key, value in car.items():
-				if key == 'controle_technique':
-					car[key] = datetime.fromtimestamp(value).strftime("%d/%m/%Y")
-		return jsonify(cars[0])
+    @app.route("/cars/<int:car_id>", methods=["GET"])
+    def get_car(car_id):
+        car = Car.query.get(car_id)
+        if car is None:
+            return Response({}, status=404, mimetype='application/json')
+        return jsonify(car.serialize)
 
-class ReparationsByCar(Resource):
-	@app.route("/reparations/<car_id>", methods=["GET"], endpoint="get")
-	def getReparationsByCar(car_id):
-		conn = db_connect.connect()
-		query = conn.execute("select id, type, kilometrage, description, adresse_garage, cout, date from reparation WHERE car_id=%d ORDER BY date" %int(car_id))
-		reparations =[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
-		for reparation in reparations:
-			for key, value in reparation.items():
-				if key == 'date':
-					reparation[key] = datetime.fromtimestamp(value).strftime("%d/%m/%Y")
-		result = {'reparations': reparations}
-		return jsonify(result)
+    @app.route("/cars", methods=["POST"])
+    def add_car():
+        data = request.get_json(force=True)
+        car = Car(data['name'],
+                  data['brand'],
+                  data['color'],
+                  data['license_plate'],
+                  datetime.datetime.strptime(data['technical_inspection'], "%d/%m/%Y"))
+        db.session.add(car)
+        db.session.commit()
+        return jsonify(car.serialize)
 
-class PostReparation(Resource): 
-	@app.route("/reparation", methods=["POST"], endpoint="post")	
-	def addReparation():
-		request_data = request.json
-		conn = db_connect.connect()
-		date = str(request_data['date']).split('/')
-		timestamp = datetime.fromisoformat(date[2] + "-" + date[1] + "-" + date[0]).timestamp()
-		query = conn.execute('INSERT INTO reparation (car_id, type, kilometrage, description, adresse_garage, cout, date) VALUES ({}, "{}", {}, "{}", "{}", {}, {})'.format(int(request_data['car_id']), str(request_data['type']), int(request_data['kilometrage']), str(request_data['description']).replace("'","\'"), str(request_data['adresse_garage']).replace("'","\'"), float(request_data['cout']), int(timestamp)))
-		result = {}
-		return jsonify(result)
+    @app.route("/cars/<int:car_id>", methods=["PUT"])
+    def update_car(car_id):
+        data = request.get_json(force=True)
+        car = Car.query.get(car_id)
+        car.name = data['name']
+        car.brand = data['brand']
+        car.color = data['color']
+        car.license_plate = data['license_plate']
+        car.technical_inspection = datetime.datetime.strptime(data['technical_inspection'], "%d/%m/%Y")
+        db.session.commit()
+        return jsonify(car.serialize)
 
-class Reparations(Resource):
-	@app.route("/reparations", methods=["GET"])
-	def getAllReparations(self):
-		conn = db_connect.connect()
-		query = conn.execute("select id, type, kilometrage, description, adresse_garage, cout, date from reparation ORDER BY date")
-		reparations =[dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]
-		for reparation in reparations:
-			for key, value in reparation.items():
-				if key == 'date':
-					reparation[key] = datetime.fromtimestamp(value).strftime("%d/%m/%Y")
-		result = {'reparations': reparations}
-		return jsonify(result)
-	
+
+class Repairments(Resource):
+    @app.route("/repairments", methods=["GET"])
+    def get_repairments():
+        repairments = Repairment.query.all()
+        return jsonify([i.serialize for i in repairments])
+
+    @app.route("/repairments", methods=["POST"])
+    def add_repairment():
+        data = request.get_json(force=True)
+        repairment = Repairment(
+                  data['type'],
+                  data['kilometers'],
+                  data['description'],
+                  data['garage_address'],
+                  data['price'],
+                  datetime.datetime.strptime(data['date'], "%d/%m/%Y"),
+                  data['car_id'])
+        db.session.add(repairment)
+        db.session.commit()
+        return jsonify(repairment.serialize)
+
+    @app.route("/repairments/<int:repairment_id>", methods=["PUT"])
+    def update_repairment(repairment_id):
+        data = request.get_json(force=True)
+        repairment = Repairment.query.get(repairment_id)
+        repairment.type = data['type']
+        repairment.kilometers = data['kilometers']
+        repairment.description = data['description']
+        repairment.garage_address = data['garage_address']
+        repairment.price = data['price']
+        repairment.date = datetime.datetime.strptime(data['date'], "%d/%m/%Y")
+        db.session.commit()
+        return jsonify(repairment.serialize)
+
+    @app.route("/repairments/<repairment_id>", methods=["DELETE"])
+    def delete_repairment(repairment_id):
+        repairment = Repairment.query.get(repairment_id)
+        db.session.delete(repairment)
+        db.session.commit()
+        return Response({}, status=204, mimetype='application/json')
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    brand = db.Column(db.String(30), unique=False, nullable=False)
+    color = db.Column(db.String(30), unique=False, nullable=False)
+    license_plate = db.Column(db.String(9), unique=True, nullable=False)
+    technical_inspection = db.Column(db.DateTime, unique=False, nullable=False)
+    repairments = db.relationship('Repairment', backref='car', lazy=True)
+
+    def __init__(self, name, brand, color, license_plate, technical_inspection):
+        self.name = name
+        self.brand = brand
+        self.color = color
+        self.license_plate = license_plate
+        self.technical_inspection = technical_inspection
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'brand': self.brand,
+            'color': self.color,
+            'license_plate': self.license_plate,
+            'technical_inspection': self.technical_inspection.strftime("%d/%m/%Y"),
+            'repairments': [repairment.serialize for repairment in self.repairments]
+        }
+
+
+
+class Repairment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(80), unique=True, nullable=False)
+    kilometers = db.Column(db.Integer, unique=True, nullable=False)
+    description = db.Column(db.String(255), unique=True, nullable=False)
+    garage_address = db.Column(db.String(255), unique=True, nullable=False)
+    price = db.Column(db.Float, unique=True, nullable=False)
+    date = db.Column(db.DateTime, unique=False, nullable=False)
+    car_id = db.Column(db.Integer, db.ForeignKey('car.id'), nullable=False)
+
+    def __init__(self, type, kilometers, description, garage_address, price, date, car_id):
+        self.type = type
+        self.kilometers = kilometers
+        self.description = description
+        self.garage_address = garage_address
+        self.price = price
+        self.date = date
+        self.car_id = car_id
+
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'type': self.type,
+            'kilometers': self.kilometers,
+            'description': self.description,
+            'garage_address': self.garage_address,
+            'price': self.price,
+            'car_id': self.car_id,
+            'date': self.date.strftime("%d/%m/%Y")
+        }
+
 
 if __name__ == '__main__':
-	app.run(port='5002')
+    app.run(port='5002')
